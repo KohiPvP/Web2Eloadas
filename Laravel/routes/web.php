922f7ec87fa988eb7c4b9844ff7aef56929ee5a8
Krisz <torokkrisztofer46@gmail.com>
2025-11-15 18:39:01 +0100

    feat: add authentication

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Middleware\Auth2;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return Inertia::render('welcome');})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {return Inertia::render('dashboard');})->name('dashboard');
+    Route::get('user', function () { return view('user'); })->name("user");
+    Route::get('admin', 'App\Http\Controllers\AdminController@adminPage');
 });
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
+
+Route::get('exercise1', function () { return view('view1'); });
+Route::get('introduction', function () { return view('introduction'); });
+
